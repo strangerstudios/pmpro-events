885a1c644f1c13943db79d54a12b7242db83ee16
Jason Coleman <33220397+ideadude@users.noreply.github.com>
2023-01-05 00:22:20 -0500

    Update modules/the-events-calendar.php

--- modules/the-events-calendar.php
+++ modules/the-events-calendar.php
@@ -218,7 +218,8 @@ function pmpro_events_tribe_events_add_require_membership_message( $post ) {
 				</style>
 			<?
 			$parent_event_url = add_query_arg( array( 'post' => intval( $parent_event_id ), 'action' => 'edit' ), admin_url( 'post.php' ) );
-			echo '<a href="' . esc_url( $parent_event_url ) . '">' . __( 'Edit the parent event for membership restrictions.', 'pmpro-events' ) . '</a>';
+			echo '<a href="' . esc_url( $parent_event_url ) . '">' . esc_html__( 'Edit the parent event for membership restrictions.', 'pmpro-events' ) . '</a>';
+
 		}
 
 	}
